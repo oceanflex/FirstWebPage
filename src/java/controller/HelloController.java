a120116e42a741578a6cb223da7bff68cd6cbee1
Zachary <Zachary@10.30.19.137>
2016-01-25 18:43:03 -0600

    created HelloService to do processing, changed controller to use the service

--- src/java/controller/HelloController.java
+++ src/java/controller/HelloController.java
@@ -29,6 +29,7 @@ public class HelloController extends HttpServlet {
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         response.setContentType("text/html;charset=UTF-8");
+        
         try (PrintWriter out = response.getWriter()) {
             String name = request.getParameter("username");
             HelloService service = new HelloService();
